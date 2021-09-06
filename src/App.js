90c4fb48b5c6feeb70165030d6ca5da3a1eee6bf
sasban <azure.saswata@gmail.com>
2021-09-06 11:06:27 +0530

    Working with multiple slices.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Fragment } from "react";
 import Counter from "./components/Counter";
+import Auth from "./components/Auth";
+import Header from "./components/Header";
 
 function App() {
-  return <Counter />;
+  return (
+    <Fragment>
+      <Header />
+      <Auth />
+      <Counter />
+    </Fragment>
+  );
 }
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -22,42 +22,26 @@ const counterSlice = createSlice({
   },
 });
 
-// const counterReducer = (state = initialState, action) => {
-//   if (action.type === "increment") {
-//     return {
-//       counter: state.counter + 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
+const initialAuthState = { isAuthenticated: false };
 
-//   if (action.type === "increase") {
-//     return {
-//       counter: state.counter + action.amount,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "decrement") {
-//     return {
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "toggle") {
-//     return {
-//       showCounter: !state.showCounter,
-//       counter: state.counter,
-//     };
-//   }
-
-//   return state;
-// };
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
 
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
